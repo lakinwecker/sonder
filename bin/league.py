5d3e713ffba10a2823e0947be830fd3e22c1a81e
michael1241 <michael@jamcupboard.co.uk>
2019-04-22 17:11:11 +0100

    Updating format to use main() as bin function calls.

--- bin/league.py
+++ bin/league.py
@@ -1,20 +1,21 @@
+#!/usr/bin/env python
 import click
 import os
 import sys
+
 sys.path.insert(0, "..")
 print(sys.path)
 
 from dotenv import load_dotenv
 
-from sonder.league.utils import get_season_game_ids
-
 @click.command()
 @click.option('--league', required=True, help='team4545, lonewolf')
 @click.option('--season', required=True, help='number for team4545, number with or without u1800 for lonewolf')
-def game_list(league, season):
+def main(league, season):
+    load_dotenv()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sonder.settings')
+    from sonder.league.utils import get_season_game_ids
     print(get_season_game_ids(league, season))
 
 if __name__ == '__main__':
-    load_dotenv()
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sonder.settings')
-    game_list()
+    main()
--- sonder/utils.py
+++ sonder/utils.py
@@ -10,3 +10,25 @@ def pgn_to_uci(pgn):
     for move in game.mainline_moves():
         moves.append(move.uci())
     return moves
+
+def import_pgn_to_db(pgn):
+    pgn_in = open(pgn)
+    while True:
+        game = chess.pgn.read_game(pgn_in)
+        if game is None:
+            break
+        into_db(game)
+
+def into_db(game):
+    from sonder.analysis.models import Game, Player
+    w, _ = Player.objects.get_or_create(username=game.headers['White'])
+    b, _ = Player.objects.get_or_create(username=game.headers['Black'])
+    exporter = chess.pgn.StringExporter(headers=True, variations=False, comments=False)
+    pgn_text = game.accept(exporter)
+    g = Game(
+        lichess_id = game.headers['Site'][-8:],
+        white_player = w,
+        black_player = b,
+        time_control = game.headers['TimeControl'])
+    g.set_pgn(pgn_text)
+    g.save()
