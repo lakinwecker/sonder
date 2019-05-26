b690c49e4218bac2d1daf5d8bea6771f6ccc01b8
Lakin Wecker <lakin@structuredabstraction.com>
2019-05-26 15:43:35 -0600

    Initial working login and styles

---
+++ sonder/frontend/models.py
@@ -0,0 +1,34 @@
+"""The front end models are primarily about oauth.
+"""
+import datetime
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class OAuth2Token(models.Model):
+    user =  models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    token_type = models.CharField(max_length=20)
+    access_token = models.CharField(max_length=1024)
+    refresh_token = models.CharField(max_length=1024)
+    # oauth 2 expires time
+    expires_at = models.DateTimeField()
+
+    def update_from_token(self, user, token):
+        assert(self.user == user or self.user is None)
+        self.user = user
+        self.token_type = token.get('token_type', 'bearer')
+        self.access_token = token.get('access_token')
+        self.refresh_token = token.get('refresh_token')
+        print(token.get('expires_at'))
+        expires_at = datetime.datetime.fromtimestamp(token.get('expires_at'))
+        self.expires_at = expires_at
+
+    def to_token(self):
+        return dict(
+            access_token=self.access_token,
+            token_type=self.token_type,
+            refresh_token=self.refresh_token,
+            expires_at=self.expires_at,
+        )
+
