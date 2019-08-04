2666c99404551e8bd05ae46b20cfbafb67653899
Lakin Wecker <lakin@structuredabstraction.com>
2019-08-03 19:45:03 -0600

    Further minor fixes

--- sonder/analysis/views.py
+++ sonder/analysis/views.py
@@ -1,6 +1,6 @@
 
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
