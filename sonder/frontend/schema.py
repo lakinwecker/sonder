a1265b601f4a318a43509ae0f07ee2d3d99b6765
Lakin Wecker <lakin@structuredabstraction.com>
2020-04-26 09:38:58 -0600

    Further formatting/linting

--- sonder/frontend/schema.py
+++ sonder/frontend/schema.py
@@ -7,26 +7,27 @@ from graphene import ObjectType, String, Field
 
 UserLoginResult = {
     "title": "UserLoginResult",
-    "type" : "object",
+    "type": "object",
     "required": ["preferences"],
-    "properties" : {
-        "preferences" : {
-            "type" : "object",
+    "properties": {
+        "preferences": {
+            "type": "object",
             "required": ["background"],
-            "properties": {
-                "background": {"type": "string"},
-            }
+            "properties": {"background": {"type": "string"},},
         }
-    }
+    },
 }
+
 
 class AuthStatus(graphene.Enum):
     AUTHORIZED = 1
     UNAUTHORIZED = 2
     ANONYMOUS = 3
 
+
 class Preferences(ObjectType):
     background = String()
+
 
 class UserStatus(ObjectType):
     status = Field(AuthStatus)
@@ -39,14 +40,9 @@ class Query(ObjectType):
     def resolve_userStatus(self, info, **kwargs):
         user = info.context.user
         if user.is_authenticated:
-            return UserStatus(
-                status=AuthStatus.AUTHORIZED,
-                preferences=None
-            )
+            return UserStatus(status=AuthStatus.AUTHORIZED, preferences=None)
         else:
-            return UserStatus(
-                status=AuthStatus.ANONYMOUS,
-                preferences=None
-            )
+            return UserStatus(status=AuthStatus.ANONYMOUS, preferences=None)
+
 
 schema = graphene.Schema(query=Query)
